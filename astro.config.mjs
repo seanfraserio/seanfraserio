5fdad1e1b2679406155f68955a07061bffdce787
Sean Fraser <sstgfraser@gmail.com>
2026-01-04 14:56:52 -0500

    Reinstall Keystatic CMS with hybrid mode and prerendered pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,25 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
+import keystatic from '@keystatic/astro';
 
 export default defineConfig({
   site: 'https://seanfraser.io',
-  output: 'static',
+  output: 'hybrid',
+  adapter: cloudflare({
+    imageService: 'passthrough',
+  }),
   integrations: [
     tailwind(),
     mdx(),
     markdoc(),
     sitemap(),
     react(),
+    keystatic(),
   ],
   markdown: {
     shikiConfig: {
