7712995e3ace5d3ef74181ceb039cb6021ea3887
Sean Fraser <sstgfraser@gmail.com>
2026-01-05 12:36:54 -0500

    Switch to GitHub storage mode for Keystatic

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -2,10 +2,8 @@ import { config, fields, collection } from '@keystatic/core';
 
 export default config({
   storage: {
-    kind: 'cloud',
-  },
-  cloud: {
-    project: 'seanfraserio/seanfraserio',
+    kind: 'github',
+    repo: 'seanfraserio/seanfraserio',
   },
   ui: {
     brand: {
